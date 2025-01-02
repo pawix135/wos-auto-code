843623337e4ed38e1fc15409875e515e4cc18ab6
pawix135 <leagueoflegendsoppl@gmail.com>
2025-01-02 03:38:18 +0100

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer className="text-center w-full py-2 mt-5">
+      &copy; 2024 Pawix. Website not associated with Whiteout Survivor.
+    </footer>
+  )
+}
